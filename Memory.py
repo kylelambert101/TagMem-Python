67cc940ed2aec2c5fa746ea8f50250f9f5fbe52c
Kyle <kylelambert101@gmail.com>
2016-06-23 00:22:35 +0000

    Altered TagMem storage format
    These versions use a dictionary instead of a list as the main memory storage structure.  I'm still working on restoring full functionality to the program. I may also consider restructuring the whole way entries are stored (i.e. using tags as dictionary keys and associating entries (id, name, value) with all necessary keys)

--- Memory.py
+++ Memory.py
@@ -2,44 +2,35 @@ from ENTRY import ENTRY
 class Memory:
     
     def __init__(self):
-        #list to store all entries (and to rule them all)
-        self.storage = []
+        #store entries in a dictionary
+        self.storage = dict()
         #IDnum to assign to next Entry
         self.nextEntryID = 0
 
     def isValidID(self, ID):
-        for entry in self.storage:
-            try:
-                int(ID)
-            except:
-                print("That was not a valid ID")
-                return
-            if int(entry.getID()) == int(ID):
-                return True
-        return False
+        return self.storage.has_key(ID)
 
-    def add(self, anEntry):
-        self.storage.append(anEntry)
+    def add(self, anEntry, newID):
+        self.storage[''+newID] = anEntry
 
-    def remove(self, entry):
-        self.storage.remove(entry)
+    def remove(self, entryID):
+        del self.storage[entryID]
 
     def assignNewID(self):
         toAssign = self.nextEntryID
         self.nextEntryID +=1
         return toAssign
-        
+      
     def printSimple(self):
-        nicknames = []
-        for entry in self.storage:
+        for entry in self.storage.values():
             print('{}: {}'.format(entry.getID(), entry.getNickName()))
-            
+          
     def printDetail(self):
-        for entry in self.storage:
+        for entry in self.storage.values():
             print()
             entry.printDetail()
             print()
-            
+          
     def printHits(hitList):
         print("Done searching")
         print("Hits:")
@@ -49,18 +40,17 @@ class Memory:
 
     def searchMatchOne(self, queries):
         hitList = []
-        for entry in self.storage:
+        for entry in self.storage.values():
             added = False
             for query in queries:
                 if ((query.lower() in entry.getTagList()) and (not added)):
                     hitList.append(entry)
                     added = True
         Memory.printHits(hitList)
-            
 
     def searchMatchAll(self, queries):
         hitList = []
-        for entry in self.storage:
+        for entry in self.storage.values():
             added = False
             match = [False]*len(queries)
             for counter in range(len(queries)):
@@ -72,7 +62,7 @@ class Memory:
 
     def searchListValues(self, queries):
         hitList = []
-        for entry in self.storage:
+        for entry in self.storage.values():
             added = False
             match = [False]*len(queries)
             for counter in range(len(queries)):
@@ -84,11 +74,7 @@ class Memory:
             each.printValue()
 
     def getByID(self, ID):
-        for entry in self.storage:
-            if entry.getID() == int(ID):
-                return entry
-        #Should print error if no entry is found with that ID
-        print("Couldn't find that entry")
+        return self.storage[''+ID]
 
     def createEntry(self, name, value, tagList):
         newID = self.assignNewID()
@@ -96,11 +82,11 @@ class Memory:
 
     def addNewEntry(self, name, value, tagList):
         toAdd = self.createEntry(name, value, tagList)
-        self.add(toAdd)
+        self.add(toAdd, toAdd.getID())
 
     def associateTags(self, existing, new):
         #for each entry, if it has existing but not new, add new
-        for entry in self.storage:
+        for entry in self.storage.values():
             if entry.hasTag(existing) and not entry.hasTag(new):
                 entry.addTag(new)
                 print("Updated Entry {}".format(entry.getID()))
